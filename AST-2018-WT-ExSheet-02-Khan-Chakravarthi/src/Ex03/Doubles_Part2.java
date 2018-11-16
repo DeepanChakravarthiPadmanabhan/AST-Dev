3a3cbb02192e464eeb5d6c53abe4c5960d48d4f3
deepan <deepangrad@gmail.com>
2018-11-17 00:23:39 +0100

    Ex03 program has been coded. Kindly code review it and raise test exceptions- Deepan

---
+++ AST-2018-WT-ExSheet-02-Khan-Chakravarthi/src/Ex03/Doubles_Part2.java
@@ -0,0 +1,69 @@
+package Ex03;
+
+public class Doubles_Part2 {
+	
+	
+	public float Product_term(int k,int n){
+		float fact = 1f;
+		float value = 1f;
+		for(int i=1; i<=n; i++){
+			if (i<k){
+				value =0.9f; 
+			}
+			else {
+				value=0.1f;
+			}
+			fact= fact*(value/(1-value));
+		}
+		return fact;
+	}
+	
+	public float POS(int k,int n){
+		int kvalue=k;
+		int nvalue=n;
+		float answer= 	1f-(1/(1+Product_term(kvalue,nvalue)));
+		return answer;
+	}
+	
+	public double Mathod2(int k,int n){
+		int kvalue=k;
+		int nvalue=n;
+		double secondterm =Math.pow((0.9/(1.0-0.9)), k-1)*Math.pow((0.1/(1.0-0.1)), (n-k+1));
+		double answer= 	1-(1/(1+secondterm));
+		return answer;
+	}
+	
+	 public static void main(String[] args)
+	 {
+		 double startTime,endTime,countTime;
+		 Doubles_Part2 one=new Doubles_Part2();
+		 float s= one.POS(10,20);
+		 System.out.printf("Time for k,n = 10,20--- %.2f", s);
+		 System.out.println("\n");
+		 
+		 startTime = System.nanoTime();
+		 s= one.POS(100,120);
+		 endTime   = System.nanoTime();
+	     countTime = endTime - startTime;
+	     System.out.println("Time for normal method "+countTime);
+		 System.out.printf("Time for k,n = 100,120--- %.2f", s);
+		 System.out.println("\n");
+		 
+		 s= one.POS(1000,1200);
+		 System.out.printf("Time for k,n = 1000,1200--- %.2f", s);
+		 System.out.println("\n");
+		 
+		 s= one.POS(10000,10200);
+		 System.out.printf("Time for k,n = 10000,10200--- %.2f", s);
+		 System.out.println("\n");
+		 
+		 startTime = System.nanoTime();
+		 double value=one.Mathod2(100,120);
+		 endTime   = System.nanoTime();
+		 countTime = endTime - startTime;
+		 System.out.println("Time for efficient method "+countTime);
+		 System.out.printf("Method 2- Time for k,n = 100,120--- ", + value);
+		 
+		 //For higher values of k, the function value equates to 1.
+	 }
+}
