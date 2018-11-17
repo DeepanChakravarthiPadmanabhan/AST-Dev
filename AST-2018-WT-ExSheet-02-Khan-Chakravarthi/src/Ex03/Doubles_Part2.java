2c3ec2b9074d8918483497f46f5b2dd5aad1b98c
Umer Khan <umer.khan@smail.inf.h-brs.de>
2018-11-17 19:41:00 +0100

    Refactored main to show statistics of executions more understandable

--- AST-2018-WT-ExSheet-02-Khan-Chakravarthi/src/Ex03/Doubles_Part2.java
+++ AST-2018-WT-ExSheet-02-Khan-Chakravarthi/src/Ex03/Doubles_Part2.java
@@ -38,8 +38,6 @@ public class Doubles_Part2 {
 		 double startTime,endTime,countTime;
 		 Doubles_Part2 one=new Doubles_Part2();
 		 float s= one.POS(10,20);
-		 System.out.printf("Time for k,n = 10,20--- %.2f", s);
-		 System.out.println("\n");
 		 
 		 startTime = System.nanoTime();
 		 s= one.POS(100,120);
@@ -49,11 +47,19 @@ public class Doubles_Part2 {
 		 System.out.printf("Time for k,n = 100,120--- %.2f", s);
 		 System.out.println("\n");
 		 
+		 startTime = System.nanoTime();
 		 s= one.POS(1000,1200);
+		 endTime   = System.nanoTime();
+	     countTime = endTime - startTime;
+	     System.out.println("Time for normal method "+countTime);
 		 System.out.printf("Time for k,n = 1000,1200--- %.2f", s);
 		 System.out.println("\n");
 		 
+		 startTime = System.nanoTime();
 		 s= one.POS(10000,10200);
+		 endTime   = System.nanoTime();
+	     countTime = endTime - startTime;
+	     System.out.println("Time for normal method "+countTime);
 		 System.out.printf("Time for k,n = 10000,10200--- %.2f", s);
 		 System.out.println("\n");
 		 
@@ -62,7 +68,24 @@ public class Doubles_Part2 {
 		 endTime   = System.nanoTime();
 		 countTime = endTime - startTime;
 		 System.out.println("Time for efficient method "+countTime);
-		 System.out.printf("Method 2- Time for k,n = 100,120--- ", + value);
+		 System.out.printf("Method 2- Time for k,n = 100,120--- %.2f", value);
+		 System.out.println("\n");
+		 
+		 startTime = System.nanoTime();
+		 value=one.Mathod2(1000,1200);
+		 endTime   = System.nanoTime();
+		 countTime = endTime - startTime;
+		 System.out.println("Time for efficient method "+countTime);
+		 System.out.printf("Method 2- Time for k,n = 1000,1200--- %.2f", value);
+		 System.out.println("\n");
+		 
+		 startTime = System.nanoTime();
+		 value=one.Mathod2(10000,10200);
+		 endTime   = System.nanoTime();
+		 countTime = endTime - startTime;
+		 System.out.println("Time for efficient method "+countTime);
+		 System.out.printf("Method 2- Time for k,n = 10000,10200--- %.2f", value);
+		 System.out.println("\n");
 		 
 		 //For higher values of k, the function value equates to 1.
 	 }
